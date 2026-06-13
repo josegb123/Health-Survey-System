9662a788c523165fdd5a78b6086f4cd2c285916c
Gabriel Oyola <oyolasjose@gmail.com>
2026-06-13 15:15:52 -0500

    fix: redirection to login on reaperture when session is closed

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,15 @@ Route::view('/', 'livewire.auth.login')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::get('users/index', Index::class)->name('users.index');
 });
 
 require __DIR__ . '/settings.php';
-
-Route::get('users/index', Index::class)->name('users.index');
-
-
-
 
 // fallback route for smart redirection
 Route::fallback(function () {
     if (Auth::check()) {
         return redirect()->route('dashboard'); // Cambia 'dashboard' por el nombre de tu ruta del panel
     }
-
     return redirect()->route('login');
 });
