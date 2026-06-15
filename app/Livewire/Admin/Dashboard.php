ea75b56118ac8917a0f7569a9fcbfa75335d5171
Gabriel Oyola <oyolasjose@gmail.com>
2026-06-15 15:57:07 -0500

    feat: dashboard class for livewire component that handle metrics

---
+++ app/Livewire/Admin/Dashboard.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Carbon\Carbon;
+use Livewire\Component;
+
+class Dashboard extends Component
+{
+    public string $period = 'month'; // Filtro por defecto
+    public string $startDate;
+    public string $endDate;
+
+    public function mount(): void
+    {
+        $this->updateDates();
+    }
+
+    public function updatedPeriod(): void
+    {
+        $this->updateDates();
+
+        // Notifica de forma reactiva al subcomponentes hijo
+        $this->dispatch('dashboard-filter-updated', [
+            'startDate' => $this->startDate,
+            'endDate' => $this->endDate
+        ]);
+    }
+
+    private function updateDates(): void
+    {
+        $this->endDate = Carbon::now()->endOfDay()->toDateTimeString();
+
+        $this->startDate = match ($this->period) {
+            'week'  => Carbon::now()->subDays(7)->startOfDay()->toDateTimeString(),
+            'year'  => Carbon::now()->subYear()->startOfDay()->toDateTimeString(),
+            default => Carbon::now()->subMonth()->startOfDay()->toDateTimeString(),
+        };
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.dashboard');
+    }
+}
