5514659096614cfb8851f28919a47b6528ffcd90
Gabriel Oyola <oyolasjose@gmail.com>
2026-06-14 21:00:07 -0500

    fix: survey answer relations with survey and survey question

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Support\Facades\URL;
@@ -73,5 +74,14 @@ class Survey extends Model
     public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
+    }
+
+    /**
+     * Summary of answers
+     * @return HasMany<SurveyAnswer, Survey>
+     */
+    public function answers(): HasMany
+    {
+        return $this->hasMany(SurveyAnswer::class);
     }
 }
--- app/Models/SurveyQuestion.php
+++ app/Models/SurveyQuestion.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 #[Fillable('survey_template_id', 'question_text', 'field_type', 'order')]
 class SurveyQuestion extends Model
@@ -19,5 +20,14 @@ class SurveyQuestion extends Model
     public function template(): BelongsTo
     {
         return $this->belongsTo(SurveyTemplate::class, 'survey_template_id');
+    }
+
+    /**
+     * Summary of answer
+     * @return HasOne<SurveyAnswer, SurveyQuestion>
+     */
+    public function answer(): HasOne
+    {
+        return $this->hasOne(SurveyAnswer::class);
     }
 }
